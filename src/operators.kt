6a7a325d6e7d999cd7d7553ffbbfbd47d3985c63
09winnie <winfredgraceekendi@gmail.com>
2021-02-28 11:28:48 +0300

    practicing invoking functions

---
+++ src/operators.kt
@@ -0,0 +1,30 @@
+fun main() {
+println("Hello Tamara")
+    modulus()
+    sum()
+    personality()
+}
+fun name(){
+    val name="Tamara"
+    println("Hello Tamara")
+}
+fun modulus(){
+    var num1=19
+    var num2=9
+    var modulus=(num1%num2)
+    println (modulus)
+
+}
+fun sum(){
+    var num1=9
+    var num2=4
+    var num3=18
+    var num4=19
+    var sum=(num1+num2+num3+num4)
+    println(sum)
+
+}
+fun personality(){
+    val personality=("I am an extroverted introvert")
+    println(personality)
+}
